8eade17c51b436d647ba73df13866fd888f363a6
Julia Friesel <julia.friesel@gmail.com>
2018-10-18 12:01:22 +0200

    Set initial state and set background color based on it

--- index.js
+++ index.js
@@ -38,16 +38,20 @@ console.log(shelf)
 // choo app
 
 const app = choo()
-app.use(setUpEventListeners)
+app.use(prepareStateAndListeners)
 app.route('/', view)
 app.route('/test-network-1', view)
 app.route('/test-network-2', view)
 app.mount('body')
 
 function view(state) {
+  const currentApp = state.activeApp
+  const colors = ['lightyellow', 'lightblue']
+  const appIndex = appIds.indexOf(currentApp)
+  const bg = `background-color:${colors[appIndex]}`
   return html`
-    <body style='background-color:lightyellow'>
-      <a id="switch-to-app-2" href="/test-network-2">Switch to other app</a><br>  
+    <body style=${bg}>
+      <button id="switch-app">Switch to other app</button><br>  
       <input type="text" id="post" name="your message"/><br>
       <button id="add-to-list">Post message</button>  
       <br>
@@ -67,7 +71,32 @@ function view(state) {
   `
 }
 
-function setUpEventListeners(state, emitter) {
+function prepareStateAndListeners(state, emitter) {
+  state.activeApp = appIds[0]
+
+  emitter.on('DOMContentLoaded', () => {
+    document.getElementById('publish').addEventListener('click', () => {
+      // sbot.publish({
+      //   type: 'hello-world'
+      // })
+    })
+
+    document.getElementById('add-to-list').addEventListener('click', () => {
+      const textField = document.getElementById('post')
+      // sbot.publish({
+      //   type: 'post',
+      //   text: textField.value
+      // })
+      textField.value = ''
+    })
+
+    document.getElementById('switch-app').addEventListener('click', () => {
+      const otherAppId = appIds.find(id => id !== state.activeApp)
+      state.activeApp = otherAppId
+      emitter.emit('render')
+    })
+  })
+
   appIds.forEach(appName => {
     const ssbConfig = config(appName)
     const keys = ssbKeys.loadOrCreateSync(path.join(ssbConfig.path, 'secret'))
@@ -80,22 +109,6 @@ function setUpEventListeners(state, emitter) {
       sbot.gossip.peers((err, peers) => {
         console.log(err)
         console.log(peers)
-      })
-
-      document.getElementById('publish').addEventListener('click', () => {
-        sbot.publish({
-          type: 'hello-world'
-        })
-      })
-
-      document.getElementById('add-to-list').addEventListener('click', () => {
-        const textField = document.getElementById('post')
-        console.log(config.caps.shs)
-        sbot.publish({
-          type: 'post',
-          text: textField.value
-        })
-        textField.value = ''
       })
 
       pull(
