93f3e58b998d01f2dcebfa022ddf263d47b6c20d
Ela Blechschmidt <ela@posteo.de>
2018-11-28 16:32:27 +0100

    add mutant to sidebar

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,40 +1,47 @@
 'use strict'
 const { div } = require('../html-helpers')
+const mutantKeys = require('mutant/keys')
 
 module.exports = function (state) {
-  const appIds = Object.keys(state.apps)
+  console.log('runs')
   const colors = ['#F9065F', '#1DA0E1', '#27A83F', '#F9B405']
   let maybeActiveStyle = {}
-  if (state.wizardActive) {
+  if (state.wizardActive()) {
     maybeActiveStyle['background-color'] = '#F9065F'
   }
+  console.log()
+  console.log(mutantKeys(state.apps))
+  const keyObs = mutantKeys(state.apps)
   return div('.blockparties', [
-    div('.list-blockparties',
-      appIds.map((id, i) => {
-        var isActive = id === state.activeApp
-        const displayId = id.slice(0, 2)
-        const currentStyle = {}
-        const color = colors[i % colors.length]
-        if (isActive && !state.wizardActive) {
-          currentStyle['background-color']= color
-          currentStyle.color = '#fff'
-          currentStyle.border = '1px solid' + color
-        } else {
-          currentStyle.color = color
-        }
-        return div('.blockparty', {
-          'ev-click': () => {
-            state.activeApp = id
-            state.wizardActive = false
-          },
-          style: currentStyle
-        }, displayId)
+    div('.list-blockparties', [
+      keyObs((appIds) => {
+        console.log(appIds)
+        appIds.map((id, i) => {
+          var isActive = id === state.activeApp()
+          const displayId = id.slice(0, 2)
+          const currentStyle = {}
+          const color = colors[i % colors.length]
+          if (isActive && !state.wizardActive()) {
+            currentStyle['background-color']= color
+            currentStyle.color = '#fff'
+            currentStyle.border = '1px solid' + color
+          } else {
+            currentStyle.color = color
+          }
+          return div('.blockparty', {
+            'ev-click': () => {
+              state.activeApp.set(id)
+              state.wizardActive.set(false)
+            },
+            style: currentStyle
+          }, displayId)
+        })
       })
-    ),
+    ]),
     div('#add-network', {
       style: maybeActiveStyle,
       'ev-click': () => {
-        state.wizardActive = true
+        state.wizardActive.set(true)
       }}, '+')
   ])
 }
