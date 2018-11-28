afd85db425df5a911ab6771263669b0293b398e6
Ela Blechschmidt <ela@posteo.de>
2018-11-28 17:40:26 +0100

    add computed

--- components/sidebar.js
+++ components/sidebar.js
@@ -2,6 +2,7 @@
 const { div } = require('../html-helpers')
 const mutantKeys = require('mutant/keys')
 const map = require('mutant/map')
+const computed = require('mutant/computed')
 
 module.exports = function (state) {
   console.log('runs')
@@ -15,23 +16,26 @@ module.exports = function (state) {
     div('.list-blockparties', [
       map(keyObs, id => {
         const i = keyObs().indexOf(id)
-        var isActive = id === state.activeApp()
         const displayId = id.slice(0, 2)
-        const currentStyle = {}
-        const color = colors[i % colors.length]
-        if (isActive && !state.wizardActive()) {
-          currentStyle['background-color']= color
-          currentStyle.color = '#fff'
-          currentStyle.border = '1px solid' + color
-        } else {
-          currentStyle.color = color
-        }
+        const currentStyleObs = computed([state.activeApp], activeApp => {
+          var isActive = id === activeApp
+          const currentStyle = {}
+          const color = colors[i % colors.length]
+          if (isActive && !state.wizardActive()) {
+            currentStyle['background-color']= color
+            currentStyle.color = '#fff'
+            currentStyle.border = '1px solid' + color
+          } else {
+            currentStyle.color = color
+          }
+          return currentStyle
+        })
         return div('.blockparty', {
           'ev-click': () => {
             state.activeApp.set(id)
             state.wizardActive.set(false)
           },
-          style: currentStyle
+          style: currentStyleObs
         }, displayId)
       })
     ]),
