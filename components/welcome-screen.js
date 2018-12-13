ec4501778c076078ccf2a4be2f77031ff6ce1b23
Julia Friesel <julia.friesel@gmail.com>
2018-12-13 17:41:08 +0100

    Remove log

--- components/welcome-screen.js
+++ components/welcome-screen.js
@@ -4,7 +4,6 @@ const { body, button } = require('../html-helpers')
 module.exports = (state) => {
   return body('welcome!',
     button({'ev-click': () => {
-      console.log('click')
       state.wizardActive.set(true)
     }}, 'create a network!')
   )
