8afc37c336b08ecca5f52d8c325a68db8394e760
Julia Friesel <julia.friesel@gmail.com>
2018-12-17 14:48:19 +0100

    accept code

--- join-network.js
+++ join-network.js
@@ -9,15 +9,10 @@ module.exports = async function (code, cb) {
   // TODO error handling for wrong input
   const port = parseInt(code.match(/:([0-9]+):/)[1])
   const [invite, appId, appName] = code.split('!')
-  console.log(port)
-  console.log(invite)
-  console.log(appId)
-  console.log(appName)
   const opts = createConfig(appName, appId, port, port + 1, appName)
   localSetup.setUpAppDir(appName, blockpartyDir, opts)
   const sbot = startSbot(opts)
-  // use sbot to accept or decline invite code
-  sbot.invite.accept(code, err => {
+  sbot.invite.accept(invite, err => {
     if (err) return cb(err)
     cb(null, 'Yeah, worked!')
   })
