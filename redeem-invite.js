c708d5eaa817f8d7f046d0365db281eb0d050510
Ela Blechschmidt <ela@posteo.de>
2018-12-17 11:22:22 +0100

    add TODO to redeemInmviteCode

--- redeem-invite.js
+++ redeem-invite.js
@@ -1,6 +1,7 @@
 'use strict'
 
 module.exports = async function redeemInviteCode(code) {
+  //TODO error handling for wrong input
   const port = code.match(/:([0-9]+):/)[1]
   const [invite, appId] = code.split('!')
   console.log(port)
