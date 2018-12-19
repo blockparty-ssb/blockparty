a14b932006bd48d26cba051022c9c860c805a312
Julia Friesel <julia.friesel@gmail.com>
2018-12-19 15:14:45 +0100

    Create keys when joining network

--- join-network.js
+++ join-network.js
@@ -1,16 +1,18 @@
 'use strict'
+const path = require('path')
+const ssbKeys = require('ssb-keys')
 const localSetup = require('./set-up-locally')
 const blockpartyDir = require('./blockparty-dir')
 const createConfig = require('./create-sbot-config')
 const startSbot = require('./server')
-
 
 module.exports = async function (code, cb) {
   // TODO error handling for wrong input
   const port = parseInt(code.match(/:([0-9]+):/)[1])
   const [invite, appId, appName] = code.split('!')
   const opts = createConfig(appName, appId, port, port + 1, appName)
-  localSetup.setUpAppDir(appName, blockpartyDir, opts)
+  const appDir = localSetup.setUpAppDir(appName, blockpartyDir, opts)
+  ssbKeys.loadOrCreateSync(path.join(appDir, 'secret'))
   const sbot = startSbot(opts)
   sbot.invite.accept(invite, err => {
     if (err) return cb(err)
