6140a2514c924a59f2d877a6e7d04402d84fde99
Julia Friesel <julia.friesel@gmail.com>
2019-01-22 11:02:34 +0100

    Delete app folder if join fails

--- join-network.js
+++ join-network.js
@@ -1,6 +1,7 @@
 'use strict'
 const path = require('path')
 const ssbKeys = require('ssb-keys')
+const rimraf = require('rimraf')
 const localSetup = require('./set-up-locally')
 const blockpartyDir = require('./blockparty-dir')
 const createConfig = require('./create-sbot-config')
@@ -19,9 +20,6 @@ module.exports = async function (code, cb) {
   }
   const [invite, appId, inviterId, appName] = inviteCodeParts
   const config = createConfig(appName, appId, port, port + 1, appName)
-  // TODO we shouldn't write the directory before we know whether the invite
-  // was successful? Or delete in error case?
-  // TODO error handling for appDir
   let appDir
   try {
     appDir = localSetup.setUpAppDir(appName, blockpartyDir, config)
@@ -34,16 +32,20 @@ module.exports = async function (code, cb) {
   config.manifest = sbot.getManifest()
   config.ownId = keys.id
   sbot.invite.accept(invite, err => {
-    if (err) return cb(err)
+    if (err) {
+      rimraf(appDir)
+      return cb(err)
+    }
     sbot.publish({
       type: "contact",
       contact: inviterId,
       following: true
     }, err => {
-      if (err) return cb(err)
+      if (err) {
+        rimraf(appDir)
+        return cb(err)
+      }
       cb(null, appName, config)
     })
   })
 }
-
-
